8011d2927804e8f10ad2e04dfe78ef5bbde7920b
Subrahmanya Manigadde <35496030+subrahmanyamanigadde@users.noreply.github.com>
2020-07-22 20:14:06 +0530

    update2

--- contact_process.php
+++ contact_process.php
@@ -16,7 +16,7 @@
     $subject = "You have a message from your Bitmap Photography.";
 
     $logo = 'img/logo.png';
-    $link = '#';
+    $link = 'subrahmanyamanigadde@gmail.com';
 
 	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
 	$body .= "<table style='width: 100%;'>";
